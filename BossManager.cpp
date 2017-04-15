5f936998c0fe48f594dc5824151ce7b85bc09b8b
Daniel-ITCR <daniel.esteban.8@hotmail.com>
2017-04-15 12:55:26 -0600

    añade class boss

---
+++ BossManager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by danielc on 15/04/17.
+//
+
+#include "BossManager.h"
+BossManager::BossManager(){
+
+};
---
+++ BossManager.h
@@ -0,0 +1,16 @@
+//
+// Created by danielc on 15/04/17.
+//
+
+#ifndef XEONWARS_BOSSMANAGER_H
+#define XEONWARS_BOSSMANAGER_H
+
+
+class BossManager {
+public:
+    BossManager();
+private:
+};
+
+
+#endif //XEONWARS_BOSSMANAGER_H
