dbf71c1706e0c720b6d867151eb7b6ee04e7bba6
jimena <jimenamaria598@gmail.com>
2017-04-19 14:29:53 -0600

    Clase enemy reader

---
+++ enemyReader.h
@@ -0,0 +1,22 @@
+//
+// Created by jimena on 19/04/17.
+//
+
+#ifndef XEONWARS_ENEMYREADER_H
+#define XEONWARS_ENEMYREADER_H
+
+#include <SFML/System/Clock.hpp>
+using namespace sf;
+
+
+class enemyReader{
+private:
+    int currentLevel;
+    int quantityOfEnemiesToRead;
+    const levelTime;
+    Clock levelClock;
+
+};
+
+
+#endif //XEONWARS_ENEMYREADER_H
