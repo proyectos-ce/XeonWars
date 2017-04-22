44c31754213d3bba5a6f39ed9015122e27f58949
Gustavo Segura <gsegura96@openmailbox.org>
2017-04-22 02:01:14 -0600

    pruebas

---
+++ powerup.cpp
@@ -0,0 +1,6 @@
+#include "powerup.h"
+
+PowerUp::PowerUp()
+{
+
+}
---
+++ powerup.h
@@ -0,0 +1,11 @@
+#ifndef POWERUP_H
+#define POWERUP_H
+#include "bullet.h"
+
+class PowerUp : Bullet
+{
+public:
+    PowerUp();
+};
+
+#endif // POWERUP_H
